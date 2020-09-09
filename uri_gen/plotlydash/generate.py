e66cdcca07f7aafbfad7d90952bd5bc665972727
Jean-Eudes Hollebecq <jehollebecq@gmail.com>
2020-09-09 11:14:40 +0200

    replace callbacks

--- uri_gen/plotlydash/generate.py
+++ uri_gen/plotlydash/generate.py
@@ -23,16 +23,6 @@ def init_dashboard(server):
 
     # Load DataFrame
     df = create_dataframe()
-    @app.callback(
-    Output(component_id='URI-path', component_property='children'),
-    [Input(component_id='hostname', component_property='value'),
-     Input(component_id="installation", component_property="value")], method=['GET', 'POST']
-    )
-    @app.callback(Output('database-table', 'children'),
-            [Input('upload-data', 'contents')],
-            [State('upload-data', 'filename'),
-            State('upload-data', 'last_modified')])
-
     # Custom HTML layout
     dash_app.index_string = html_layout
 
@@ -44,8 +34,66 @@ def init_dashboard(server):
         ],
         id='dash-container'
     )
-    app.config.suppress_callback_exceptions = True
-    app.config.update({
+
+    @dash_app.callback(
+        Output(component_id='URI-path', component_property='children'),
+        [Input(component_id='hostname', component_property='value'),
+        Input(component_id="installation", component_property="value")], method=['GET', 'POST']
+    )
+    def update_output_div(username, instalName):
+        return 'URI path: {}'.format(str(username)+"/"+str(instalName))
+
+
+    def parse_contents(contents, filename, date):
+        content_type, content_string = contents.split(',')
+
+        decoded = base64.b64decode(content_string)
+        try:
+            if 'csv' in filename:
+                # Assume that the user uploaded a CSV file
+                df = pd.read_csv(
+                    io.StringIO(decoded.decode('utf-8')))
+            elif 'xls' in filename:
+                # Assume that the user uploaded an excel file
+                df = pd.read_excel(io.BytesIO(decoded))
+        except Exception as e:
+            print(e)
+            return html.Div([
+                'There was an error processing this file.'
+            ])
+
+        return html.Div([
+            html.H5(filename),
+            html.H6(datetime.datetime.fromtimestamp(date)),
+
+            dash_table.DataTable(
+                data=df.to_dict('records'),
+                columns=[{'name': i, 'id': i} for i in df.columns]
+            ),
+
+            html.Hr(),  # horizontal line
+
+            # For debugging, display the raw contents provided by the web browser
+            html.Div('Raw Content'),
+            html.Pre(contents[0:200] + '...', style={
+                'whiteSpace': 'pre-wrap',
+                'wordBreak': 'break-all'
+            })
+        ])
+    @dash_app.callback(Output('database-table', 'children'),
+            [Input('upload-data', 'contents')],
+            [State('upload-data', 'filename'),
+            State('upload-data', 'last_modified')])
+
+    def update_output(list_of_contents, list_of_names, list_of_dates):
+        if list_of_contents is not None:
+            children = [
+                parse_contents(c, n, d) for c, n, d in
+                zip(list_of_contents, list_of_names, list_of_dates)]
+            return children
+
+    dash_app.config.suppress_callback_exceptions = True
+    dash_app.config.update({
     # as the proxy server will remove the prefix
     'routes_pathname_prefix': ''
 
@@ -54,7 +102,7 @@ def init_dashboard(server):
     , 'requests_pathname_prefix': ''
     })
     return dash_app.server
-
+   
 
 def create_data_table(df):
     """Create Dash datatable from Pandas DataFrame."""
